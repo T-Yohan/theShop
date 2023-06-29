8f9d7e5c10c22f6700f9b06088dc2eeb9d26827e
T-Yohan <yohanturlet@outlook.fr>
2023-06-29 14:07:04 -0400

    detail images

--- app/View/Components/CarouselDetail2.php
+++ app/View/Components/CarouselDetail2.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class CarouselDetail extends Component
+class CarouselDetail2 extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class CarouselDetail extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.carousel-detail');
+        return view('components.carousel-detail2');
     }
 }
--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -27,35 +27,8 @@
 
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-6">
             <div class="flex flex-col md:flex-row -mx-4">
-                <div class="md:flex-1 px-4">
-                    <div x-data="{ image: 1 }" x-cloak>
-                        <div class="h-64 md:h-80 rounded-lg bg-gray-100 mb-4">
-
-                            @forelse ($product->carouselImage as $key => $carouselImage)
-                                <div x-show="image==={{ $key + 1 }}"
-                                    class="h-64 md:h-80 rounded-lg bg-gray-100 mb-4 flex items-center justify-center">
-                                    <img src="{{ Storage::url($carouselImage) }}" alt="">
-                                </div>
-                            @empty
-                            @endforelse
-                        </div>
-
-                        <div class="flex -mx-2 mb-4">
-
-                            @forelse ($product->carouselImage as $key => $carouselImage)
-                                <div class="flex-1 px-2">
-                                    <button x-on:click="image = {{ $key + 1 }}"
-                                        :class="{ 'ring-2 ring-indigo-300 ring-inset': image === {{ $key + 1 }} }"
-                                        class="focus:outline-none w-full rounded-lg h-24 md:h-32 bg-gray-100 flex items-center justify-center">
-                                        <img src="{{ Storage::url($carouselImage) }}" alt="">
-                                    </button>
-                                </div>
-                            @empty
-                            @endforelse
-                        </div>
-
-                    </div>
-                </div>
+                {{-- carousel --}}
+                <x-carousel-detail :images="$product->carouselImage" />
                 <div class="md:flex-1 px-4">
                     <h2 class="mb-2 leading-tight tracking-tight font-bold text-gray-800 text-2xl md:text-3xl">
                         {{ $product->name }}
